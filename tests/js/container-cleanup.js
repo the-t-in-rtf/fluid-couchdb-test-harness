60940b6248837c9a2c0141fd0133bc63e7960e8e
Tony Atkins <tony@raisingthefloor.org>
2019-09-05 10:26:38 +0200

    GPII-3531: Added a check to prevent a crash when running with `GPII_TEST_COUCH_USE_VAGRANT` set.

--- tests/js/container-cleanup.js
+++ tests/js/container-cleanup.js
@@ -5,7 +5,7 @@ var gpii = fluid.registerNamespace("gpii");
 require("../..");
 
 if (fluid.get(process, "env.GPII_TEST_COUCH_USE_EXTERNAL")) {
-    fluid.log("Skipping container cleanup for 'external' worker.");
+    fluid.log("Skipping container cleanup for 'external' worker(s).");
 }
 else {
     gpii.test.couchdb.worker.couch({
@@ -17,12 +17,14 @@ else {
         }
     });
 
-    gpii.test.couchdb.worker.lucene({
-        removeContainer: true,
-        listeners: {
-            "onCreate.shutdown": {
-                func: "{that}.shutdown"
+    if (!fluid.get(process, "env.GPII_TEST_COUCH_USE_VAGRANT")) {
+        gpii.test.couchdb.worker.lucene({
+            removeContainer: true,
+            listeners: {
+                "onCreate.shutdown": {
+                    func: "{that}.shutdown"
+                }
             }
-        }
-    });
+        });
+    }
 }
